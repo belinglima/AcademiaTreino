e4f499f5bc0ce4aca22b8a5a432be249f8e10402
Salomão Beling <belinglima@gmail.com>
2019-11-13 00:43:53 -0300

    Updates

---
+++ precache-manifest.6e2a7122c6e0a5cfbbbbdcd677d9184b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1563d63c9e7712beacea4be7696e35",
+    "url": "/AcademiaTreino/index.html"
+  },
+  {
+    "revision": "82e8d4221d3d1b306390",
+    "url": "/AcademiaTreino/static/css/main.0ea2200a.chunk.css"
+  },
+  {
+    "revision": "0f29a6f31c09610905a1",
+    "url": "/AcademiaTreino/static/js/2.2b4cb7ed.chunk.js"
+  },
+  {
+    "revision": "82e8d4221d3d1b306390",
+    "url": "/AcademiaTreino/static/js/main.f9aca802.chunk.js"
+  },
+  {
+    "revision": "b5cda56a2f7766996056",
+    "url": "/AcademiaTreino/static/js/runtime-main.1ba0d91d.js"
+  },
+  {
+    "revision": "2b178f2320dd1489a1b37a6bc8e0fa66",
+    "url": "/AcademiaTreino/static/media/img.2b178f23.png"
+  }
+]);
